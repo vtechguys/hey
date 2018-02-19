e1fe09808ce1ddb51c333204b1d88e62d2ca1cd5
Aniket Jha <vtechguys@gmail.com>
2018-02-19 11:40:03 +0530

    Update controller.js

--- controller.js
+++ controller.js
@@ -11,16 +11,16 @@ app.controller("mainController",function($scope,$http){
 
 
 
-    $scope.processLogInData=()=>{
-        console.log("inside process data $email is",($scope.emailLogIn),"password is ",($scope.passwordLogIn));
-        $http.post("signin.php",{
-            'email':$scope.emailLogIn,
-            'password':$scope.passwordLogIn
-        }).then(function(data){
-            console.log("data sent successfully",data,"data");
-        }).catch(function(){
-            console.log("error in sending data");
-        })
-    }
+//     $scope.processLogInData=()=>{
+//         console.log("inside process data $email is",($scope.emailLogIn),"password is ",($scope.passwordLogIn));
+//         $http.post("signin.php",{
+//             'email':$scope.emailLogIn,
+//             'password':$scope.passwordLogIn
+//         }).then(function(data){
+//             console.log("data sent successfully",data,"data");
+//         }).catch(function(){
+//             console.log("error in sending data");
+//         })
+//     }
 
 })
