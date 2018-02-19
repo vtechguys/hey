25a91acbf0edd12b8bd022965362ca38bd2964f9
Aniket Jha <vtechguys@gmail.com>
2018-02-19 11:36:15 +0530

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,22 +1,16 @@
 app.controller("mainController",function($scope,$http){
-    // $scope.registerMe=(email,password,matchPasswordStatus)=>{
-    //     $scope.signedIn=formFactory.register(email,password);
-    //     console.log($scope.signedIn);
-       
-    // };
-    // $scope.matchPassword=(password,confirmPassword)=>{
-    //     $scope.matchPasswordStatus=formFactory.matchPassword(password,confirmPassword);
-    //     console.log("matchPasswordStatus is",$scope.matchPasswordStatus);
-    // }
+    
 
 
-console.log("inside process data $email is",($scope.emailLogIn),"password is ",($scope.passwordLogIn),"running controller");
+console.log("inside mainController $email is",($scope.emailLogIn),"password is ",($scope.passwordLogIn));
     $scope.processLogInData=()=>{
         console.log("inside process data $email is",($scope.emailLogIn),"password is ",($scope.passwordLogIn));
         $http.post("signin.php",{
             'email':$scope.emailLogIn,
             'password':$scope.passwordLogIn
-        }).then(functtion (data){console.log(data);}).catch(function (error){console.log("error occured",error);});
+        }).then(function (data){console.log(data);
+        console.log("inside process data $email is",($scope.emailLogIn),"password is ",($scope.passwordLogIn));
+    }).catch(function (error){console.log("error occured",error);});
     }
 
 })
